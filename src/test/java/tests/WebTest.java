96bb79922e73f9e9d92a0c6cb4f51d6828e1f285
Admin <bogtinys20@gmail.com>
2021-02-01 21:33:25 +0300

    fixed

--- src/test/java/tests/WebTest.java
+++ src/test/java/tests/WebTest.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import config.WebConfigHelper;
 import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,9 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
-public class WebTest  {
+public class WebTest {
 
     @Test
     @DisplayName("Web Test")
@@ -21,6 +23,10 @@ public class WebTest  {
         String searchItem = WebConfigHelper.getSearchItem();
         String searchResult = WebConfigHelper.getSearchResult();
         Configuration.remote = WebConfigHelper.getWebdriverRemote();
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = WebConfigHelper.getBrowserName();
+        Configuration.browserVersion = WebConfigHelper.getBrowserVersion();
+        Configuration.startMaximized = true;
 
         open(searchURl);
         $(byName("q")).setValue(searchItem).pressEnter();
